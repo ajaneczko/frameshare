925df68333958187ca481ccc9d1b557a2675e1ed
Arkadiusz Janeczko <ajaneczko1@gmail.com>
2015-12-31 14:37:09 +0100

    Allow users to comment projects on disqus

--- client/app/dashboard/project/project.controller.js
+++ client/app/dashboard/project/project.controller.js
@@ -1,7 +1,9 @@
 'use strict';
 
 angular.module('syfApp')
-  .controller('ProjectCtrl', function ($scope, Project, $stateParams) {
+  .controller('ProjectCtrl', function ($scope, Project, $stateParams, $location) {
+      $scope.currentPath = 'http://localhost:9000' + $location.path();
+      console.log($scope.currentPath)
       Project.query({ slug: $stateParams.slug }).$promise.then(function(projects) {
         $scope.project = projects[0];
       });
