4714732d8a27f7667463ed43e28b646573b24227
Arkadiusz Janeczko <ajaneczko1@gmail.com>
2015-12-31 14:50:56 +0100

    Fix for disqus

--- client/app/dashboard/project/project.controller.js
+++ client/app/dashboard/project/project.controller.js
@@ -3,7 +3,6 @@
 angular.module('syfApp')
   .controller('ProjectCtrl', function ($scope, Project, $stateParams, $location) {
       $scope.currentPath = 'http://localhost:9000' + $location.path();
-      console.log($scope.currentPath)
       Project.query({ slug: $stateParams.slug }).$promise.then(function(projects) {
         $scope.project = projects[0];
       });
