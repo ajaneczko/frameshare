8bb24829c5c5ea26dea54ecead95da7c8b05be52
Arkadiusz Janeczko <ajaneczko1@gmail.com>
2016-01-04 18:35:28 +0100

    Change title only when author of project

--- client/app/dashboard/project/project.controller.js
+++ client/app/dashboard/project/project.controller.js
@@ -1,13 +1,15 @@
 'use strict';
 
 angular.module('syfApp')
-  .controller('ProjectCtrl', function ($window, $scope, Project, $q, $stateParams, $location) {
+  .controller('ProjectCtrl', function ($window, $scope, Auth, Project, $q, $stateParams, $location) {
       var requestFinished = $q.defer();
 
       $scope.currentPath = 'http://localhost:9000' + $location.path();
       $scope.projectId = ''
       $scope.project = ''
       $scope.newTitle = ''
+      $scope.projectAuthor = ''
+      $scope.isUserAuthor = false
 
       Project.query({ slug: $stateParams.slug }).$promise.then(function(projects) {
         $scope.project = projects[0];
@@ -16,18 +18,29 @@ angular.module('syfApp')
 
       requestFinished.promise.then(function() {
         $scope.projectId = $scope.project._id;
+        $scope.projectAuthor = $scope.project.author
+
+        $scope.checkIfAuthor = function() {
+          if ($scope.project.author != null) {
+            return $scope.projectAuthor._id === Auth.getCurrentUser()._id
+
+          }
+          else
+            console.log("This project has no author")
+        }
+        $scope.isUserAuthor = $scope.checkIfAuthor()
       });
 
       $scope.isEdit = false
       $scope.showEditInput = false
 
-      $scope.toggleEdit = function() {
-        $scope.isEdit = $scope.isEdit === false ? true: false;
-      };
-
       $scope.startEditing = function () {
-        console.log($scope.projectId)
-        $scope.showEditInput = $scope.showEditInput === false ? true: false;
+        if ($scope.isUserAuthor === true && $scope.projectAuthor != null) {
+          console.log($scope.projectId)
+          $scope.showEditInput = $scope.showEditInput === false ? true: false;
+        }
+        else
+          return
       }
 
       $scope.updateTitle = function (title) {
